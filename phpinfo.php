4580ad034d273187cc4c46a7843d6488e0bf422f
Diogo Diciti <diogodiciti538@gmail.com>
2024-11-19 21:58:11 -0300

    criamos o arquivo para verficar as funcionalidades do php.

---
+++ phpinfo.php
@@ -0,0 +1,11 @@
+<?php
+// Inicio do script (php).
+
+echo phpinfo();
+// Não esquecer do ponto e virgula acima caso contrario = erro.
+
+exit;
+// fim da execução do script php.
+
+
+echo "Ola mundo!";
