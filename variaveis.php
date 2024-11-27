10bc3163eae607f66a4683d355f1abc2297a82dd
DiogoDiciti <diogodiciti538@gmail.com>
2024-11-26 21:38:32 -0300

    tipos de dados php

---
+++ variaveis.php
@@ -0,0 +1,21 @@
+<?php
+
+const CPF = '12345678901'
+const VELOCIDADE_DA_LUZ = '320k'
+
+// tipos de dados primitivos do PHP .
+$string = "Um texto qualquer";
+$string = "joao do caminhao"; // override = substituir oque existia.
+$nomePessoa = "Diogo" // style guide Camel case
+$nome_pessoa = "Diogo2" // style guide Snake Case
+$interiro = 2;
+$float = 2.5;
+$double = 2.5468;
+$char = 'a';
+
+// Tipos de dados estruturais
+$array = array();
+$array_short[]
+
+$objeto = new stdClass(); // POO - OOP
+$classe = new stdClass();
