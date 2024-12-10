af2273c8c71eec386194129947b145b62368bb9b
DiogoDiciti <diogodiciti538@gmail.com>
2024-12-10 19:55:38 -0300

    uptadate

--- tabuada.php
+++ tabuada.php
@@ -1,28 +1,30 @@
 <?php 
 
-            $numero = 6;
-            echo "Tabuada do número 6<br>" . $numero . "<br>";
-            echo "<br>6 x 1 = " .  $numero * 1;
-            echo "<br>6 x 2 = " .  $numero * 2;
-            echo "<br>6 x 3 = " .  $numero * 3;
-            echo "<br>6 x 4 = " .  $numero * 4;
-            echo "<br>6 x 5 = " .  $numero * 5;
-            echo "<br>6 x 6 = " .  $numero * 6;
-            echo "<br>6 x 7 = " .  $numero * 7;
-            echo "<br>6 x 8 = " .  $numero * 8;
-            echo "<br>6 x 9 = " .  $numero * 9;
-            echo "<br>6 x 10 = " . $numero * 10;
+    $numero = 6;
+        
+        echo "Tabuada do número 6<br>" . $numero . "<br>";
+        echo "<br>6 x 1 = " .  $numero * 1;
+        echo "<br>6 x 2 = " .  $numero * 2;
+        echo "<br>6 x 3 = " .  $numero * 3;
+        echo "<br>6 x 4 = " .  $numero * 4;
+        echo "<br>6 x 5 = " .  $numero * 5;
+        echo "<br>6 x 6 = " .  $numero * 6;
+        echo "<br>6 x 7 = " .  $numero * 7;
+        echo "<br>6 x 8 = " .  $numero * 8;
+        echo "<br>6 x 9 = " .  $numero * 9;
+        echo "<br>6 x 10 = " . $numero * 10;
 
-            $numero = 7;
-            echo "<br><br>Tabuada do número 7<br>" . $numero . "<br>";
-            echo "<br>7 x 1 = " .  $numero * 1;
-            echo "<br>7 x 2 = " .  $numero * 2;
-            echo "<br>7 x 3 = " .  $numero * 3;
-            echo "<br>7 x 4 = " .  $numero * 4;
-            echo "<br>7 x 5 = " .  $numero * 5;
-            echo "<br>7 x 6 = " .  $numero * 6;
-            echo "<br>7 x 7 = " .  $numero * 7;
-            echo "<br>7 x 8 = " .  $numero * 8;
-            echo "<br>7 x 9 = " .  $numero * 9;
-            echo "<br>7 x 10 = " . $numero * 10;
-        ?>
+    $numero = 7;
+        
+        echo "<br><br>Tabuada do número 7<br>" . $numero . "<br>";
+        echo "<br>7 x 1 = " .  $numero * 1;
+        echo "<br>7 x 2 = " .  $numero * 2;
+        echo "<br>7 x 3 = " .  $numero * 3;
+        echo "<br>7 x 4 = " .  $numero * 4;
+        echo "<br>7 x 5 = " .  $numero * 5;
+        echo "<br>7 x 6 = " .  $numero * 6;
+        echo "<br>7 x 7 = " .  $numero * 7;
+        echo "<br>7 x 8 = " .  $numero * 8;
+        echo "<br>7 x 9 = " .  $numero * 9;
+        echo "<br>7 x 10 = " . $numero * 10;
+?>
