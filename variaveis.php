15bdfbcaaef2558b3a72a4232e4df45540385ec7
DiogoDiciti <diogodiciti538@gmail.com>
2024-12-10 19:16:47 -0300

    variaveis

--- variaveis.php
+++ variaveis.php
@@ -1,13 +1,13 @@
 <?php
 
-const CPF = '12345678901'
-const VELOCIDADE_DA_LUZ = '320k'
+const CPF = '12345678901';
+const VELOCIDADE_DA_LUZ = '320k';
 
 // tipos de dados primitivos do PHP .
 $string = "Um texto qualquer";
 $string = "joao do caminhao"; // override = substituir oque existia.
-$nomePessoa = "Diogo" // style guide Camel case
-$nome_pessoa = "Diogo2" // style guide Snake Case
+$nomePessoa = "Diogo"; // style guide Camel case
+$nome_pessoa = "Diogo2"; // style guide Snake Case
 $interiro = 2;
 $float = 2.5;
 $double = 2.5468;
@@ -15,7 +15,7 @@ $char = 'a';
 
 // Tipos de dados estruturais
 $array = array();
-$array_short[]
+$array_short =[];
 
 $objeto = new stdClass(); // POO - OOP
 $classe = new stdClass();
