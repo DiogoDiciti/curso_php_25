828ec3712c6b43eb29e08148b855973f0b755db6
DiogoDiciti <diogodiciti538@gmail.com>
2024-12-17 19:05:18 -0300

    update

--- primos.php
+++ primos.php
@@ -14,28 +14,30 @@
 9 / 8 (9 -1) $numero - 1
 */
 
-<?php
-//Descobrir se o número é primo ou não.
+<br>
+    <div class="form">
+        <?php
+        //Descobrir se o número é primo ou não.
 
-$numero = $_POST['number'] ?? null;
+        $numero = $_POST['number'] ?? null;
 
-if($numero <= 1) {
-    echo "Números menores ou iguais a 1 não são considerados primos.";    
-    exit;
-}
+        if($numero <= 1) {
+            echo "Números menores ou iguais a 1 não são considerados primos.";    
+            exit;
+        }
 
-$divisores = 0;
+        $divisores = 0;
 
-for($i = 2; $i < $numero; $i++){
-    if ($numero % $i == 0){ //$i é o divisor (dividindo sempre por 2)
-        echo "O número " . $numero . " pode ser divido por $i.<br />"; //quais números dividiram $numero. 
-        $divisores++;
-        } 
-    }
-    if ($divisores == 0) {
-        echo "É primo!";
-    } else {
-        echo "Portanto " .$numero . " não é primo!<br> Ele possui $divisores divisor(es) além de 1 e ele mesmo.";
-    }
-?>
-
+        for($i = 2; $i < $numero; $i++){
+            if ($numero % $i == 0){ //$i é o divisor (dividindo sempre por 2)
+                echo "O número " . $numero . " pode ser divido por $i.<br />"; //quais números dividiram $numero. 
+                $divisores++;
+                } 
+            }
+            if ($divisores == 0) {
+                echo "É primo!";
+            } else {
+                echo "Portanto " .$numero . " não é primo!<br> Ele possui $divisores divisor(es) além de 1 e ele mesmo.";
+            }
+        ?>
+</div>
