f31f9f944cc191156242995053f8bad46234cc0d
DiogoDiciti <diogodiciti538@gmail.com>
2024-12-17 19:15:40 -0300

    Update

--- primos.php
+++ primos.php
@@ -1,39 +1,33 @@
 <?php
+ 
 /**
- * Exercicio2:
- * Dado um numero informado  pelo usuario
- * informar se ele é um numero primo.
+ * impressão de números primos de 1 a 100
+ *
+ * Números primos são aqueles que só são divisíveis por 1 e por ele mesmo. Logo
+ * ele possui apenas 2 divisores.
  */
-
- // 1, 3, 5, 7, 9
-
- /*
-    9 / 2
-    9 / 3
-    ...
-    9 / 8 
-*/
-
-$numero = 9;
-
-// (9 -1) $numero - 1
-$limitador = $numero - 1;
-
-$primo = true;
-
-for ($i = 2; $i <= $limitador; $i++ ) {
-
-    // break
-    if ($resto == 0) {
-        $primo = false;
-        break;
+  
+ 
+// loop de 1 a 100
+for($i = 1; $i <= 100; $i++)
+{
+    // variavel que armazena o número de divisores de um número
+    $divisores = 0;
+     
+    // recupera o nº atual no loop e, a partir dele, o decrementa até chegar a 1 
+    for($j = $i; $j >= 1; $j--)
+    {
+        // se o número do 1º loop for divisível por algum número anterior a ele, ou seja, resto 0 
+        // incrementa o número de divisores
+        if (($i % $j) == 0) {
+            $divisores++;
+        }
     }
-    
-}
-
-
-if ($primo) {
-    echo "<br>O $numero é primo.";
-} else {
-    echo "<br>O $numero NÃO é primo.";
+     
+    // se o número do loop principal tiver exatamente 2 divisores
+    // (lembre-se, nº primos tem somente 2 divisores: 1 e ele próprio), exibe o nº primo
+    if ($divisores == 2)
+    {
+        echo $i . ', ';
+    }
 }
