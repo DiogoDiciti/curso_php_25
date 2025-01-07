22e5d5b8ac9790265fb20d77b8f7c40029292e0a
DiogoDiciti <diogodiciti538@gmail.com>
2025-01-07 19:21:43 -0300

    update

--- calendario.php
+++ calendario.php
@@ -1,24 +1,5 @@
-
-/**
- * Criar um algoritimo que simula o funcionamento de um calendario 
- * Variaveis: $data '31/01/2025'
- * validar a data.
- * validar também se o ano é Bissexto.
- * Dica: If, else,
- */
-
- /** 3 1 / 0 1 / 2 0 2 5 -> $dia = substr($data, 0 , 2);
-  *  0 1 2 3 4 5 6 7 8 9 -> $mes = substr($data, 3 , 2);
-  *                      -> $ano = substr($data, 6 , 4);
- */ 
-
- $Data = 31/01/2025
-
-$dia = substr ($data, 0 , 2);
-$mes = substr ($data, 3 , 2);
-$ano = substr ($data, 6 , 4);
-
 <?php
+
 $data = "31/01/2025"; // Definindo a data corretamente como string
 
 // Extraindo o dia, mês e ano
