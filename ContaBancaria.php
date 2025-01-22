e7127654457dc2f1f36ec0f8e4bfab51a588a77d
DiogoDiciti <diogodiciti538@gmail.com>
2025-01-21 21:37:11 -0300

    Update

---
+++ ContaBancaria.php
@@ -0,0 +1,14 @@
+<?php
+
+class ContaBancaria {
+    public $titular = "";
+    public $destinatario = "";
+    public $saldo = 0;
+    public $historico = [];
+
+    public function sacar() {}
+    public function depositar() {}
+    public function pix () {}
+    public function verSaldo() {}
+    public function extrato() {}
+}
